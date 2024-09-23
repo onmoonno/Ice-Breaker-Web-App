924527dd9a6cf82280e3a5a3049b7c6e6f7c9d11
Yunxia Zhang <onvanessano@gmail.com>
2024-09-22 23:30:19 -0700

    Initial project

---
+++ ice_breaker.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+import os
+if __name__ == "__main__":
+    load_dotenv()
+    print("Hello LangChain")
+    print(os.environ['COOL_API_KEY'])   # access the environment variable through os.environ dict
+
